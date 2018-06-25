cd00005272de4992ee5da7ffdf19146bfadd63c1
Patrick Killean <pat.killean@gmail.com>
2018-06-25 06:39:14 -0400

    spinoff sampling code

--- src/main/clj/fress/api.clj
+++ src/main/clj/fress/api.clj
@@ -1,12 +1,7 @@
 (ns fress.api
   (:require [clojure.data.fressian :as fressian]
             [clojure.java.io :as io])
-  (:import [java.io ByteArrayOutputStream ByteArrayInputStream EOFException]
-           [java.nio ByteBuffer]
-           [org.fressian.impl RawInput Codes]
-           [org.fressian.handlers WriteHandler ReadHandler]
-           [org.fressian FressianWriter StreamingWriter FressianReader Writer Reader]
-           [org.fressian.impl ByteBufferInputStream BytesOutputStream]))
+  (:import [org.fressian.handlers WriteHandler ReadHandler]))
 
 (defn string->bytes ^bytes [^String s] (.getBytes s "UTF-8"))
 (defn bytes->string ^String [bytes] (String. bytes "UTF-8"))
@@ -31,12 +26,9 @@
       (let [bytes (.getBytes (.-s u) "UTF-8")
             raw-out (w->raw w)
             length (count bytes)]
-        (println "writing ...." *write-utf8-tag*)
-        (if *write-utf8-tag*
-          (do
-            (println "writing tag!")
-            (.writeTag w "utf8" 2))
-          (.writeCode w (int 191))) ;<= client can read either
+        (if *write-utf8-tag* ;<= client can read either
+          (.writeTag w "utf8" 2)
+          (.writeCode w (int 191)))
         (.writeCount w length)
         (.writeRawBytes raw-out bytes 0 length)))))
 
@@ -48,10 +40,10 @@
     (read [_ rdr tag component-count]
       (let [length (int (.readInt rdr))
             offset (int 0)
-            byteBuffer (byte-array length)
+            bytes (byte-array length)
             raw-in (rdr->raw rdr)]
-        (.readFully raw-in byteBuffer offset length)
-        (bytes->string byteBuffer)))))
+        (.readFully raw-in bytes offset length)
+        (bytes->string bytes)))))
 
 (def write-handlers
   (-> (merge {utf8 {"utf8" utf8-writer}} fressian/clojure-write-handlers)
@@ -61,36 +53,6 @@
 (def read-handlers
   (-> (merge {"utf8" utf8-reader} fressian/clojure-read-handlers)
       fressian/associative-lookup))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; from fressian/src/test/org/fressian/api.clj
-
-(defn ^ByteBuffer bytestream->buf
-  "Return a readable buf over the current internal state of a
-   BytesOutputStream."
-  [^BytesOutputStream stream]
-  (ByteBuffer/wrap (.internalBuffer stream) 0 (.length stream)))
-
-(defn byte-buffer-seq
-  "Return a lazy seq over the remaining bytes in the buffer.
-   Not fast: intented for REPL usage.
-   Works with its own duplicate of the buffer."
-  [^ByteBuffer bb]
-  (lazy-seq
-   (when (.hasRemaining bb)
-     (let [next-slice (.slice bb)]
-       (cons (.get next-slice) (byte-buffer-seq next-slice))))))
-
-(defn ^ByteBuffer byte-buf
-  "Return a byte buffer with the fressianed form of object.
-   See fressian for options."
-  [obj & {:keys [handlers footer]}]
-  (let [BYTES-os (BytesOutputStream.)
-        write-handlers (or handlers write-handlers)
-        writer (fressian/create-writer BYTES-os :handlers write-handlers)]
-    (.writeObject writer obj)
-    (when footer (.writeFooter writer))
-    (bytestream->buf BYTES-os)))
 
 (defn read-batch
   "Read a fressian reader fully (until eof), returning a (possibly empty)
@@ -103,137 +65,3 @@
           objects
           (recur (conj objects obj)))))))
 
-(extend ByteBuffer
-  io/IOFactory
-  (assoc io/default-streams-impl
-    :make-input-stream (fn [x opts] (io/make-input-stream
-                                     (ByteBufferInputStream. x) opts))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn bytevec [obj & {:keys [handlers footer]}]
-  (vec (byte-buffer-seq (byte-buf obj :handlers handlers :footer footer))))
-
-(defn bytes->rdr
-  [bytes]
-  (let [in (if (= (type bytes) java.nio.HeapByteBuffer)
-             bytes
-             (byte-array bytes))]
-    (fressian/create-reader (io/input-stream in) :handler read-handlers)))
-
-(defn ->rdr [o] (-> o byte-buf bytes->rdr))
-(defn ->raw-in [o] (-> o byte-buf bytes->rdr rdr->raw))
-
-(defn raw-in->ubytes [raw]
-  (loop [acc []]
-    (let [byte (try
-                 (.readRawByte raw)
-                 (catch java.io.IOException _ false))]
-      (if-not byte
-        acc
-        (recur (conj acc byte))))))
-
-(defn ubytevec [obj & {:keys [handlers footer]}]
-  (let [bytes (byte-buf obj :footer footer)
-        rdr (bytes->rdr bytes)
-        raw (rdr->raw rdr)]
-    (raw-in->ubytes raw)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;js MAX_SAFE_INT etc
-
-
-(defn typed-array? [o]
-  (let [c (class o)]
-    (or
-     (= c (Class/forName "[Z")) ;=> BOOLEAN_ARRAY
-     (= c (Class/forName "[B"))
-     (= c (Class/forName "[S"))
-     (= c (Class/forName "[I"))
-     (= c (Class/forName "[J")) ;=> LONG_ARRAY
-     (= c (Class/forName "[F"))
-     (= c (Class/forName "[D"))
-     (= c (Class/forName "[Ljava.lang.Object;")))))
-
-(defmacro sample
-  ([form & {:keys [footer tag-utf8] :or {footer false}}]
-   (binding [*write-utf8-tag* tag-utf8]
-     (let [value (if (symbol? form) form (eval form))
-           bytes (mapv long (bytevec value :footer footer))
-           ; ubytes (mapv long (ubytevec value :footer footer))
-           base {:form (pr-str form)
-                 :bytes bytes
-                 :byte-count (count bytes)
-                 :footer footer
-                 ; :ubytes ubytes
-                 ; :ubyte-count (count ubytes)
-                 }]
-       (cond
-         (or (typed-array? value) (bytes? value))
-         (assoc base :input (eval (second form)))
-
-         (utf8? value) ;need tree-seq with value transform for nested
-         (assoc base :tag? *write-utf8-tag* :value (.-s value))
-
-         (symbol? value)
-         `(assoc ~base
-                 :value '(quote ~value)
-                 :form (quote ~form) #_(str "'" (name (quote ~value))))
-
-         (instance? java.net.URI value)
-         (assoc base :input (.toString value))
-
-         :else
-         (assoc base :value value))))))
-
-(defmacro sample-each [coll] (vec (for [item coll]  `(sample ~item))))
-
-(defmacro uri-samples [coll]
-  (vec (for [item coll]  `(sample (java.net.URI. ~item)))))
-
-(deftype Person [ ^String firstName ^String  lastName]
-  Object
-  (toString [this] (str "Person " firstName " " lastName)))
-
-(defn write-person []
-  (let [BYTES-os (BytesOutputStream.)
-        tag "org.fressian.Examples.Person"
-        person-writer (reify WriteHandler
-                        (write [_ w person]
-                          (println tag)
-                          (.writeTag w tag 2)
-                          (.writeObject w (.-firstName person))
-                          (.writeObject w (.-lastName person))))
-        handlers (-> (merge {Person {tag person-writer}} fressian/clojure-write-handlers)
-                     fressian/associative-lookup
-                     fressian/inheritance-lookup)
-        writer (fressian/create-writer BYTES-os :handlers handlers)]
-    (.writeObject writer (->Person "jonny" "greenwood")) ;<= triggers struct caching
-    (.writeObject writer (->Person "thom" "yorke")) ;<= written with cache reference code
-    (bytestream->buf BYTES-os)))
-
-(def bunchOfData #{1 false "hello" (java.util.Date.) -42})
-
-(defn write-cached []
-  (let [BYTES-os (BytesOutputStream.)
-        handlers (-> fressian/clojure-write-handlers
-                     fressian/associative-lookup
-                     fressian/inheritance-lookup)
-        writer (fressian/create-writer BYTES-os :handlers handlers)]
-    (.writeObject writer bunchOfData true) ;<= triggers priorityCache
-    (.writeObject writer bunchOfData true) ;<= written with cache reference code
-    (bytestream->buf BYTES-os)))
-
-(defrecord Book [author title])
-
-(defn class-sym
-  "Returns the class name of inst as a symbol."
-  [^Object inst]
-  (-> inst (.getClass) (.getName) symbol))
-
-(defn record-sample []
-  (let [book (->Book "Borges" "El jardín de senderos que se bifurcan")]
-    {:bytes (bytevec book)
-     :author (:author book)
-     :title (:title book)
-     :class-sym (class-sym book)}))

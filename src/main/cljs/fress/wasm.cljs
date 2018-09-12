d68141a618645233d2d3e15d56b56979b424b1e0
Patrick Killean <pat.killean@gmail.com>
2018-09-12 05:18:16 -0400

    read-all now with automatic free

--- src/main/cljs/fress/wasm.cljs
+++ src/main/cljs/fress/wasm.cljs
@@ -1,6 +1,9 @@
 (ns fress.wasm
   (:require [fress.api :as api]
+            [fress.reader :as r]
             [fress.impl.codes :as codes]
+            [fress.impl.buffer :as buf]
+            [fress.impl.raw-input :as rawIn]
             [fress.util :as util]))
 
 (defn wasm-module? [o] (instance? js/WebAssembly.Instance o))
@@ -8,7 +11,7 @@
 (defn assert-fress-mod! [Mod]
   (assert (wasm-module? Mod))
   (assert (some? (.. Mod -exports -fress_alloc)))
-  ; (assert (some? (.. Mod -exports -fress_free)))
+  (assert (some? (.. Mod -exports -fress_free)))
   (assert (some? (.. Mod -exports -memory))))
 
 (defn write-bytes [Mod bytes] ;=> ptr
@@ -19,14 +22,32 @@
     (.set view bytes ptr)
     ptr))
 
-(defn read-all [Mod ptr]
+
+; will break on imported memory, add :memory option or check imports
+(defn read-all
+  "Given a WASM module, a pointer, and opts, read off fressian objects and
+   automatically free the used memory.
+     - Call this synchronously after obtaining the ptr and before any other
+       calls on the same module/memory
+     - right now opts is :handlers only
+     - no :checksum? (not in rust yet)
+     - no :name->map-ctor (no records in rust yet)"
+  [Mod ptr & {:keys [handlers] :or {handlers nil}}]
   (assert-fress-mod! Mod)
   (assert (util/valid-pointer? ptr))
-  (let [view (js/Uint8Array. (.. Mod -exports -memory -buffer))
-        result (fress.api/read-all view :offset ptr)]
-    ;; ...free....
-    (if (== codes/ERROR (aget view ptr))
-      [result]
-      [nil result])))
+  (let [memory (or (.. Mod -exports -memory) (.. Mod -imports -memory)) ; env?
+        _(assert (some? memory))
+        view (js/Uint8Array. (.-buffer memory))
+        rdr (r/reader view :offset ptr :handlers handlers)
+        result (fress.api/read-batch rdr)
+        bytes_read (rawIn/getBytesRead (get rdr :raw-in))
+        ret (if (== codes/ERROR (aget view ptr))
+              [result]
+              [nil result])]
+    ((.. Mod -exports -fress_free) ptr bytes_read) ;; dealloc after reading
+    ret))
+
+
+; (defonce _write-buffer)
 
 (defn write [v]) ;; stringify-keys

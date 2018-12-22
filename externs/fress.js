e5875d567479d3e9203dca76c43b8b3bf7978e29
Patrick Killean <pat.killean@gmail.com>
2018-12-22 14:21:20 -0500

    externs

---
+++ externs/fress.js
@@ -0,0 +1,14 @@
+/**
+ * @param {!number} length
+ * @return {!number} ptr
+ */
+WebAssembly.Module.exports.fress_alloc = function(length) {};
+
+/**
+ * @param {!number} ptr
+ * @param {!number} length
+ */
+WebAssembly.Module.exports.fress_dealloc = function(ptr, length) {};
+
+WebAssembly.Module.exports.fress_init = function() {};
+

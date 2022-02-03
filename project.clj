ded940a9e8b0029eaba29ef0911458e055ebf825
pat <pat.killean@gmail.com>
2022-02-03 11:10:00 -0400

    rework deps.edn

--- project.clj
+++ project.clj
@@ -1,38 +1,11 @@
-;  https://clojureverse.org/t/combining-tools-deps-with-leiningen/658/4
-(require '[clojure.edn :as edn])
-
-; {foo {:mvn/verion "string"}
-;  bar {:mvn/version "string"}... } => [[foo "string" :exculsions [...]] ...]
-(defn deps->vec [deps]
-  (into []
-    (map
-      (fn [[dep {:keys [:mvn/version exclusions]}]]
-        (cond-> [dep version]
-          exclusions (conj :exclusions exclusions))))
-    deps))
-
-(def raw-deps (edn/read-string (slurp "deps.edn")))
-(def config   (edn/read-string (slurp "config.edn")))
-
-(let [proj-deps (deps->vec (get raw-deps :deps))
-      src-paths (vec (get raw-deps :paths))
-      test-paths (get-in raw-deps [:aliases :test :extra-paths])
-      dev-deps (deps->vec (get-in raw-deps [:aliases :dev :extra-deps]))
-      profiles (for [[alias-key {:keys [builds]}] (get config :aliases)]
-                 (when (some? builds)
-                   (let [deps (get-in raw-deps [:aliases alias-key :extra-deps])]
-                     [alias-key {:dependencies (deps->vec deps)
-                                 :cljsbuild {:builds builds}}])))]
-  (defproject fress "0.3.2"
-    :description "Fressian for clojure(script) and WASM"
-    :url "https://github.com/pkpkpk/fress"
-    :repositories [["clojars" {:sign-releases false}]]
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :plugins [[lein-cljsbuild "1.1.8"]]
-    :jar-exclusions [#"^wasm-test/.*|public/.*"]
-    :dependencies ~proj-deps
-    :source-paths ~src-paths
-    :test-paths ~test-paths
-    :aot ~(get config :aot)
-    :profiles ~(into {} profiles)))
+(defproject fress "0.3.3"
+  :description "Fressian for clojure(script) and WASM"
+  :url "https://github.com/pkpkpk/fress"
+  :repositories [["clojars" {:sign-releases false}]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/main/clj" "src/main/cljs"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/data.fressian "1.0.0"]]
+  :jar-exclusions [#"^wasm-test/.*|public/.*"]
+  :aot [fress.impl.bytestream])
